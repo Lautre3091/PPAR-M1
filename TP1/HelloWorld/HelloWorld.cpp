0861e9ea54947f0d5629e331cfcf0298f97c8ad8
Gweltaz GESTIN <gestin.gweltaz@gmail.com>
2015-02-06 14:09:38 +0100

    HelloWord ok + Matrix

---
+++ TP1/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,19 @@
+#include <omp.h>
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    int nthreads, tid;
+    #pragma omp parallel private(nthreads, tid)
+    {
+        tid = omp_get_thread_num();
+        printf("Hello World du thread = %d\n", tid);
+
+        if (tid == 0){
+            nthreads = omp_get_num_threads();
+            printf("Nombre de threads = %d\n", nthreads);
+        }
+
+    }
+    return 0;
+}
